404ab5a6ba2b0303a9e01cc5780b58d578ac9f85
Philipp Stucki <philipp@qwert.ch>
2013-02-27 21:45:04 +0100

    clean up the formula loader dummy

--- Assetic/XsltFormulaLoader.php
+++ Assetic/XsltFormulaLoader.php
@@ -7,17 +7,9 @@ use PHST\Bundle\XsltBundle\XsltEngine;
 
 class XsltFormulaLoader implements FormulaLoaderInterface
 {
-    protected $engine;
-
-    public function __construct(XsltEngine $engine)
-    {
-        $this->engine = $engine;
-    }
-
     public function load(ResourceInterface $resource)
     {
         $formulae = array();
         return $formulae;
     }
-
 }
